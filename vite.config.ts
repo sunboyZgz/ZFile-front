8e5fee0cea1139b0d232cfcf5c664e7ee21523fb
zhugezhen_glb <zhugezhen_glb@hanzhou.com>
2021-06-02 23:17:21 +0800

    do something with alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
+import type { UserConfig, AliasOptions } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
+import TSConfig from './tsconfig.json'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [vue()],
-})
+// export default defineConfig({
+// 	plugins: [vue()],
+// })
+
+function pathResolve(dir: string) {
+	return resolve(process.cwd(), '.', dir)
+}
+
+export default (): UserConfig => {
+	const {
+		compilerOptions: { paths },
+	} = TSConfig
+
+	function resolveAlias(paths: Record<string, string[]>): AliasOptions {
+		// let resolvePaths: Record<string, string>
+		const alias = Object.keys(paths).map(key => ({
+			find: new RegExp(key.split('*').join('')),
+			replacement: pathResolve(paths[key][0].split('*').join('')) + '/',
+		}))
+		console.log(alias)
+		return alias
+	}
+	return {
+		resolve: {
+			alias: resolveAlias(paths),
+		},
+		plugins: [vue()],
+	}
+}
